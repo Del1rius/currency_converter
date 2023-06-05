47cafd36c10985ba8ad7dfdb1b2dc401e60d3413
Del1rius <timothy.aidan.barry@gmail.com>
2023-06-05 14:48:12 +0200

    finished for now

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-let currency = document.getElementById("currency")
-let currency2 = document.getElementById("currency2")
+let currency = document.getElementById("currency").value
+let currency2 = document.getElementById("currency2").value
 let result = document.getElementById("result")
 let result2 = document.getElementById("result2")
 
 
 function forwardConverter () {
-    
-    let zar = (parseFloat(result.value)*19.23)
-    result2.value = parseFloat(zar)
+    if (document.getElementById("zar") === document.getElementById("usd")) {
+        let zar = (parseFloat(result.value)*19.23)
+        result2.value = parseFloat(zar)
+    } 
+    else if (currency.document.getElementById("zar") === currency2.document.getElementById("eur")) {
+            let zarToEur = (parseFloat(result.value)*20.73)
+            result2.value = parseFloat(zarToEur)
+    }
+    else if (currency.document.getElementById("zar") === currency2.document.getElementById("gbp")) {
+            let zarToPound = (parseFloat(result.value)*24.02)
+            result2.value = parseFloat(zarToPound)
+    }false;
 
 }
 
@@ -16,3 +25,22 @@ function reverseConverter () {
     result.value = parseFloat(usd.toFixed(2))
 
 }
+
+// function allCalcs() {
+//     switch(currency===currency2){
+//         case 1:
+//             let zarToUsd = (parseFloat(result.value)*19.23)
+//             result2.value = parseFloat(zarToUsd)
+//             break;
+//         case 2: 
+            
+//             break;
+//         case 3:
+            
+//             break;
+//         default: 
+//             text = "Enter a value! "
+//     }
+
+
+// }
